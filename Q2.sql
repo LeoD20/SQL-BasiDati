f9f1702fc77958cb68f6e2db5a38f821a128e938
LeoD20 <leonardo.dolcetta.2001@gmail.com>
2022-05-08 16:11:20 +0200

    v2

--- Q2.sql
+++ Q2.sql
@@ -1,17 +1,17 @@
 --ES 1)
+
 --a)Per ciascun giudice che non ha mai presieduto nessuna causa di diffamazione (TipologiaCausa =
 --Diffamazione’) nell’anno 2020, visualizzare codice fiscale, cognome e numero di tipologie di cause
 --diverse che ha presieduto.
 
 SELECT G1.CodFiscale, Cognome, COUNT(DISTINCT TipologiaCausa) AS NumTipologiaCausa
 FROM GIUDICE G1, CAUSA C1
-WHERE G1.CodFiscale = C1.CodFiscale AND
-G1.CodFiscale NOT IN (SELECT CodFiscale
-                     FROM CAUSA
-                     WHERE TipologiaCausa = 'Diffamazione'
-                     AND (DATE(DataInizio) <= '2020/12/31'
-                     AND (Date(DataFine) >= '2020/01/01'
-                     OR DataFine IS NULL)))
+WHERE G1.CodFiscale = C1.CodFiscale
+  AND G1.CodFiscale NOT IN (SELECT CodFiscale
+                            FROM CAUSA
+                            WHERE TipologiaCausa = 'Diffamazione'
+                              AND  DATE(DataInizio) <= '2020/12/31'
+                              AND (Date(DataFine) >= '2020/01/01' OR DataFine IS NULL))
 GROUP BY G1.CodFiscale, Cognome;
 
 --b) Considerando le cause ancora in corso presiedute da un giudice che (complessivamente nel corso
@@ -20,12 +20,13 @@ GROUP BY G1.CodFiscale, Cognome;
 
 SELECT C1.CodCausa, MAX(Data) AS UltimaUdienza
 FROM CAUSA C1, UDIENZA U1
-WHERE DataFine IS NULL AND C1.CodCausa = U1.CodCausa
-AND CodFiscale IN (SELECT CodFiscale
-                   FROM CAUSA C2, UDIENZA U2
-                   WHERE C2.CodCausa = U2.CodCausa
-                   GROUP BY CodFiscale
-                   HAVING COUNT(DISTINCT CodTribunale) >= 3)
+WHERE DataFine IS NULL
+  AND C1.CodCausa = U1.CodCausa
+  AND CodFiscale IN (SELECT CodFiscale
+                     FROM CAUSA C2, UDIENZA U2
+                     WHERE C2.CodCausa = U2.CodCausa
+                     GROUP BY CodFiscale
+                     HAVING COUNT(DISTINCT CodTribunale) >= 3)
 GROUP BY C1.CodCausa;
 
 --c) Visualizzare nome, cognome e data di nascita di ciascun giudice che ha presieduto udienze presso
@@ -34,26 +35,29 @@ GROUP BY C1.CodCausa;
 
 SELECT G1.CodFiscale, Nome, Cognome, DataNascita
 FROM GIUDICE G1, CAUSA C1, UDIENZA U1
-WHERE G1.CodFiscale = C1.CodFiscale AND C1.Codcausa = U1.CodCausa
-AND CodTribunale IN (SELECT CodTribunale
-                     FROM CAUSA C2, UDIENZA U2
-                     WHERE C2.CodCausa = U2.CodCausa
-                     AND TipologiaCausa = 'Divorzio'
-                     GROUP BY CodTribunale
-                     HAVING COUNT(DISTINCT C2.CodCausa) >= 50)
+WHERE G1.CodFiscale = C1.CodFiscale
+  AND C1.Codcausa = U1.CodCausa
+  AND CodTribunale IN (SELECT CodTribunale
+                       FROM CAUSA C2, UDIENZA U2
+                       WHERE C2.CodCausa = U2.CodCausa
+                        AND TipologiaCausa = 'Divorzio'
+                       GROUP BY CodTribunale
+                       HAVING COUNT(DISTINCT C2.CodCausa) >= 50)
 GROUP BY G1.CodFiscale, Nome, Cognome, DataNascita
 HAVING COUNT(DISTINCT CodTribunale) = (SELECT COUNT(DISTINCT CodTribunale)
                                        FROM CAUSA C3, UDIENZA U3
                                        WHERE C3.CodCausa = U3.CodCausa
-                                       AND TipologiaCausa = 'Divorzio'
-                                       AND CodTribunale IN (SELECT CodTribunale
-                                                            FROM CAUSA C4, UDIENZA U4
-                                                            WHERE C4.CodCausa = U4.CodCausa
-                                                            AND TipologiaCausa = 'Divorzio'
-                                                            GROUP BY CodTribunale
-                                                            HAVING COUNT(DISTINCT C4.CodCausa) >= 50))
+                                        AND TipologiaCausa = 'Divorzio'
+                                        AND CodTribunale IN (SELECT CodTribunale
+                                                             FROM CAUSA C4, UDIENZA U4
+                                                             WHERE C4.CodCausa = U4.CodCausa
+                                                              AND TipologiaCausa = 'Divorzio'
+                                                             GROUP BY CodTribunale
+                                                             HAVING COUNT(DISTINCT C4.CodCausa) >= 50))
+
 
 --ES 2)
+
 --a) Per ciascuna palestra di Torino presso cui sono state effettuate lezioni di Judo (NomeS = ‘Judo’) da
 --almeno 5 istruttori diversi, ma non è stata effettuata nessuna lezione di Yoga (NomeS = ‘Yoga’),
 --visualizzare il nome della palestra e il numero di istruttori diversi che hanno effettuato lezioni
@@ -62,14 +66,16 @@ HAVING COUNT(DISTINCT CodTribunale) = (SELECT COUNT(DISTINCT CodTribunale)
 SELECT NomeP, COUNT(DISTINCT CodFiscale)
 FROM PALESTRA P1, LEZIONE L1
 WHERE P1.CodP = L1.CodP  AND P1.Citta = 'Torino'
-AND P1.CodP NOT IN (SELECT CodP
-                    FROM LEZIONE L2, SPECIALITA S1
-                    WHERE L2.CodS = S1.CodS AND NomeS = 'Yoga')
-AND P1.CodP IN (SELECT CodP
-                FROM LEZIONE L3, SPECIALITA S2
-                WHERE L3.CodS = S2.CodS AND NomeS = 'Judo'
-                GROUP BY CodP
-                HAVING COUNT(DISTINCT CodFiscale) >= 5)
+  AND P1.CodP NOT IN (SELECT CodP
+                      FROM LEZIONE L2, SPECIALITA S1
+                      WHERE L2.CodS = S1.CodS
+                        AND NomeS = 'Yoga')
+  AND P1.CodP IN (SELECT CodP
+                  FROM LEZIONE L3, SPECIALITA S2
+                  WHERE L3.CodS = S2.CodS
+                    AND NomeS = 'Judo'
+                  GROUP BY CodP
+                  HAVING COUNT(DISTINCT CodFiscale) >= 5)
 GROUP BY P1.CodP, NomeP
 
 --b) Per ciascun istruttore che ha solo svolto lezioni di Yoga, visualizzare il nome, l’indirizzo e la citta
@@ -78,17 +84,19 @@ GROUP BY P1.CodP, NomeP
 SELECT CodFiscale, NomeP, Indirizzo, Citta
 FROM PALESTRA P1, LEZIONE L1
 WHERE P1.CodP = L1.CodP
-AND CodFiscale NOT IN (SELECT CodFiscale
-                       FROM LEZIONE L2, SPECIALITA S1
-                       WHERE L2.CodS = S1.CodS AND NomeS <> 'Yoga')
+  AND CodFiscale NOT IN (SELECT CodFiscale
+                         FROM LEZIONE L2, SPECIALITA S1
+                         WHERE L2.CodS = S1.CodS
+                          AND NomeS <> 'Yoga')
 GROUP BY CodFiscale, P1.CodP
 HAVING COUNT(*) = (SELECT MAX(NumLezioni)
                    FROM (SELECT CodFiscale, P2.CodP, COUNT(*) AS NumLezioni
                          FROM PALESTRA P2, LEZIONE L3
                          WHERE P2.CodP = L3.CodP
-                         AND CodFiscale NOT IN (SELECT CodFiscale
-                                                FROM LEZIONE L4, SPECIALITA S2
-                                                WHERE L4.CodS = S2.CodS AND NomeS <> 'Yoga')
+                          AND CodFiscale NOT IN (SELECT CodFiscale
+                                                 FROM LEZIONE L4, SPECIALITA S2
+                                                 WHERE L4.CodS = S2.CodS
+                                                  AND NomeS <> 'Yoga')
                          GROUP BY CodFiscale, P2.CodP) AS LxP
                    WHERE L1.CodFiscale = LxP.CodFiscale)
 
@@ -97,8 +105,9 @@ HAVING COUNT(*) = (SELECT MAX(NumLezioni)
 
   SELECT NomeT, Cognome, COUNT(DISTINCT CodS)
   FROM ISTRUTTORE I1, LEZIONE L1, PALESTRA P1
-  WHERE I1.CodFiscale = L1.CodFiscale AND P1.CodP = L1.CodP
-  AND I1.Citta = P1.Citta
+  WHERE I1.CodFiscale = L1.CodFiscale
+    AND P1.CodP = L1.CodP
+    AND I1.Citta = P1.Citta
   GROUP BY I1.CodFiscale, NomeT, Cognome, I1.Citta
   HAVING COUNT(DISTINCT P1.CodP) = (SELECT COUNT(DISTINCT CodP)
                                     FROM PALESTRA P2
